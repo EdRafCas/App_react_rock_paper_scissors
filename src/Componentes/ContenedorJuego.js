c9067e367d39aa3ec955d600595abb4ad1debb72
EdRafCas <Eduardoo.rafael@gmail.com>
2021-08-05 22:13:39 -0300

    creating buttons

--- src/Componentes/ContenedorJuego.js
+++ src/Componentes/ContenedorJuego.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
-import {ReactComponent as Triangulo} from './../images/bg-triangle.svg'
-import {ReactComponent as LogoPapel} from './../images/icon-paper.svg'
+import {ReactComponent as Triangulo} from './../images/bg-triangle.svg';
+import {ReactComponent as LogoPapel} from './../images/icon-paper.svg';
+import {ReactComponent as LogoTijera} from './../images/icon-scissors.svg';
+import {ReactComponent as LogoPiedra} from './../images/icon-rock.svg';
+
 
 
 const ContenedordelJuego = styled.div`
@@ -24,7 +27,7 @@ const FondoTriangulo = styled(Triangulo)`
 const ContenedorOpcionA= styled.div`
       display:flex;
       position:fixed;
-      background: linear-gradient(to top, hsl(230, 89%, 62%), hsl(230, 89%, 65%));
+      background: linear-gradient(hsl(230, 89%, 65%), hsl(230, 89%, 62%));
       width: 10rem;
       height:10rem;
       border-radius:50%;
@@ -34,40 +37,58 @@ const ContenedorOpcionA= styled.div`
       margin-top:-15rem;
       margin-left:-20rem;
 `
-const ContenedorPapel= styled.div`
+const Btn = styled.button`
+      display:block;
       width: 8rem;
       height:8rem;
-      background: white;
+      background: #fff;
       box-shadow: 0px 5px inset;
       border-radius:50%;
       border: 1px solid hsl(217, 16%, 45%);
       margin: auto;
       display:flex;
+      align-items: center;
+      justify-content: center;
+      text-align:center;
+      cursor:pointer;
+      
+      svg{
+            height:50%;
+            width:auto;
+      }
+
+     
 `
 
-const Papel = styled(LogoPapel)`
-      width:59;
-      height:71;
-      margin:auto;
-      
+const Papel = styled(LogoPapel)`      
+`
+const Piedra =styled(LogoPiedra)`
+`
+const Tijera = styled(LogoTijera)`
 `
 
 const ContenedorOpcionB= styled.div`
+      display:flex;
       position:fixed;
+      background: linear-gradient(hsl(349, 70%, 56%), hsl(349, 71%, 52%));
       width: 10rem;
       height:10rem;
       border-radius:50%;
-      border: 0.25rem solid hsl(217, 16%, 45%);
+      border: 1px solid hsl(217, 16%, 45%);
+      box-shadow: 0px 7px 0px 0px ;
       z-index:1;
       margin-top:15rem;
       margin-left:0rem;
 `
 const ContenedorOpcionC= styled.div`
+      display:flex;
       position:fixed;
+      background: linear-gradient(hsl(40, 84%, 53%), hsl(39, 89%, 49%));
       width: 10rem;
       height:10rem;
       border-radius:50%;
-      border: 0.25rem solid hsl(217, 16%, 45%);
+      border: 1px solid hsl(217, 16%, 45%);
+      box-shadow: 0px 7px 0px 0px ;
       z-index:1;
       margin-top:-15rem;
       margin-left:20rem;
@@ -80,18 +101,23 @@ const ContenedorJuego = () => {
             <ContenedordelJuego>
                   <FondoTriangulo/>
                         <ContenedorOpcionA>
-                              <ContenedorPapel>
-                                    <Papel 
-                                          width="70"
-                                          height="90"
-                                          viewBox= "1 2 55 65"
+                              <Btn> 
+                                    <Papel                              viewBox="0 0 49 59"
                                     />
-                              </ContenedorPapel>
+                              </Btn>
                         </ContenedorOpcionA>
                   <ContenedorOpcionB>
-
+                              <Btn> 
+                                    <Piedra                              viewBox="0 0 49 59"
+                                    />
+                              </Btn>
                   </ContenedorOpcionB>
-                  <ContenedorOpcionC  />
+                  <ContenedorOpcionC>
+                              <Btn> 
+                                    <Tijera                            viewBox="0 0 49 59"
+                                    />
+                              </Btn>
+                  </ContenedorOpcionC>
             </ContenedordelJuego>
       );
 }
